96fd67231875b2a83c5eef4c7b8ecbd6787d83ae
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2024-02-15 10:39:50 +0100

    Update offers.js

--- src/offers.js
+++ src/offers.js
@@ -1,22 +1,27 @@
 import Card from './card.js';
 
+const Offers = (data) => {
+    const { title, description } = data;
+    const { packages } = data[2];
+    const { name, price, checked } = data.packages[0];
 
 
-function Offers() {
-    return (
+    function list() {
+        return (
 
-        <div className="offer">
-            <div className="offer-section">
-                <h2 className="offer-section__title">{title}</h2>
-                <p className="offer-section__description">{desciption}</p>
-                <div className="box">
-                    {packages.map((item, index) => (
-                        <Card key={index} label={item.name} price={item.price} perks={item.checked} />
-                    ))}
+            <div className="offer">
+                <div className="offer-section">
+                    <h2 className="offer-section__title">{title}</h2>
+                    <p className="offer-section__description">{description}</p>
+                    <div className="box">
+                        {packages.map((item, index) => (
+                            <Card key={index} label={name} price={price} perks={checked} />
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Offers;
