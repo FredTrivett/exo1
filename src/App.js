89ca8ccee6d9bb2b00d2cc4d9075636cb0c618a6
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2024-02-15 10:53:26 +0100

    switch

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ function App() {
           <h2 className="offer-section__title">Prix et option à l'achat</h2>
           <p className="offer-section__description">Découvrez l'avenir de la mobilité avec des prix compétitifs et des
             options d'achat flexibles chez Auto Innovation.</p>
+
+          <div className='switch-container'>
+            <p className='switch-text'>Buy</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+            <p className='switch-text'>Lease</p>
+          </div>
+
           <div className="box">
             <Offers />
           </div>
