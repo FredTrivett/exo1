1ff17035443f5b66dae7bf78fc1b355bfc4898c9
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2024-02-15 11:47:37 +0100

    update

---
+++ src/offers.jsx
@@ -0,0 +1,39 @@
+import Card from './card.jsx';
+import { useState } from 'react';
+
+export default function Offers({ title, description, plans }) {
+    const [inputChecked, setInputChecked] = useState(false);
+
+    const handleInputChange = () => {
+        setInputChecked(!inputChecked);
+    };
+
+    return (
+        <div className='offer'>
+            <div className='offer-section'>
+                <h2 className='offer-section__title'>{title}</h2>
+                <p className='offer-section__description'>{description}</p>
+                <div className='offer-section__description'>
+                    <label className="switch">
+                        <input onClick={handleInputChange} type="checkbox" />
+                        <span className="slider round"></span>
+                    </label>
+                </div>
+
+                <div className='box'>
+                    {plans.map((plan, index) => {
+
+                        return (
+                            <Card
+                                key={index}
+                                label={plan.name}
+                                price={inputChecked ? plan.price.leasing : plan.price.buy}
+                                advantages={plan.advantages}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
